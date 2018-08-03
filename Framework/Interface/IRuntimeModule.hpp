4a37b42eeb50e0e35997029e27129174ef9b85a2
unknown <gzliuyu2014@game.ntes>
2018-08-03 11:20:09 +0800

    before graphic commit

---
+++ Framework/Interface/IRuntimeModule.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Interface.hpp"
+
+namespace My
+{
+
+Interface IRuntimeModule
+{
+public:
+	virtual ~IRuntimeModule(){};
+
+	virtual int Initialize() = 0;
+	virtual void Finalize() = 0;
+	virtual void Tick() = 0;
+
+};
+
+}
+
